f609e1978efa54dfdea7c7e2231f142f91a0e762
Julian Khatibi <ajkhatibi@gmail.com>
2016-10-11 15:40:06 -0700

    finishing count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,19 +5,29 @@ var user = 0
 var chances = 0
 var songs = ["Nirvana", "Green Day", "Blink 182", "Red Hot Chili Pepers", "Smaching Pumpkins"]
 var randomImage = images[Math.floor(Math.random() * images.length)];
-var randomSong = songs[Math.floor(Math.random() * songs.length)];
+var randomSong;
+var empty;
 
+
+
+
+function updateHtml(){
+	document.getElementById("blanks").innerHTML = empty.join(" ");
+}
 //starting the game
 
 function startFunction(){
-	var empty = []
+
+	empty = []
+	randomSong = songs[Math.floor(Math.random() * songs.length)];
 	for (var i = 0; i < randomSong.length; i++) {
 	empty.push(" _ ");
-	};
+	}
 
+
+	updateHtml()
 	document.getElementById("numberofchar").innerHTML = randomSong.length;
 	document.getElementById("nameofsong").innerHTML = randomSong;
-	document.getElementById("blanks").innerHTML = empty.join(" ");
 
 	var forImage = randomSong.charAt(0);
 
@@ -33,9 +43,54 @@ function startFunction(){
 		document.getElementById("imagecontent").src = images[4];
 
 	};
+	//get letters to appear in the game
+
+
 };
 
-//get letters to appear in the game
+
+	
+	document.onkeyup = function gameFunction (event) {
+		function counter(){
+			var count = 0
+	for (var i = 0; i < lowerCaseRandomSong.length; i++){
+		if (i <= count){
+			count++
+		} else{
+			startFunction()
+		}
+
+	}
+	
+}
+		var user = String.fromCharCode(event.keyCode).toLowerCase(); 
+		var lowerCaseRandomSong = randomSong.toLowerCase();
+		var indexSong = lowerCaseRandomSong.indexOf(user);
+
+
+		if (indexSong > -1){
+			for (var i = 0; i < lowerCaseRandomSong.length; i++){
+				if (lowerCaseRandomSong[i] === user){
+					empty[i] = user;
+					console.log(empty);
+				}
+			}
+
+		} 
+		else 
+				{
+				chances++
+				document.getElementById("chances").innerHTML = chances
+				console.log(chances);
+				}
+		updateHtml();
+		counter();
+
+	};
+	
+
+
+
 
 
 
