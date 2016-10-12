5050710c5df19cdb469443021b9fa212e606e846
Julian Khatibi <ajkhatibi@gmail.com>
2016-10-12 15:30:22 -0700

    final 3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //generate random image
 var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var images = ["images/nirvana.jpg", "images/green-day.jpg", "images/blink-182.png", "images/red-hot.jpg", "images/smaching.jpg"]
+var images = ["assets/images/nirvana.jpg", "assets/images/green-day.jpg", "assets/images/blink-182.png", "assets/images/red-hot.jpg", "assets/images/smaching.jpg"]
 var user = 0
 var chances = 0
 var songs = ["Nirvana", "Green Day", "Blink 182", "Red Hot Chili Pepers", "Smaching Pumpkins"]
